04400bfca423d4bbd1143fc443c78dcc9cfd8a65
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-10-06 20:44:30 +0200

    feat(dashboard): Updated dashboard styling

--- financez/src/app/dashboard/page.tsx
+++ financez/src/app/dashboard/page.tsx
@@ -1,8 +1,18 @@
-export default function About() {
+export default function Dashboard() {
     return (
-        <div>
-            <h1>Dashboard page</h1>
-            <a href="/">Return</a>
+        <div className="flex flex-col h-6 w-full">
+            <div className="flex p-4">
+                <h1 className="font-ambroise font-demi text-2xl">FINANCEZ</h1>
+            </div>
+            <div className="flex flex-row">
+                <div className="flex flex-col w-[240px] h-full gap-1 p-4">
+                    <a className="text-white-5" href="/dashboard">Dashboard</a>
+                    <a className="text-white-5" href="/dashboard">Dashboard</a>
+                    <a className="text-white-5" href="/dashboard">Dashboard</a>
+                    <a className="text-white-5" href="/dashboard">Dashboard</a>
+                </div>
+                <div className="flex bg-black-5 rounded w-full h-full"></div>
+            </div>
         </div>
     );
 }
