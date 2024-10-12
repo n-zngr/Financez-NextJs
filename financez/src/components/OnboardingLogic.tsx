704602eb116690857e779b8330ce79ca30e7161e
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-10-12 20:55:28 +0200

    feat(onboarding): Created Onboarding6 SignUp page without API logic, temporarily removed showcase pages

--- financez/src/components/OnboardingLogic.tsx
+++ financez/src/components/OnboardingLogic.tsx
@@ -9,6 +9,7 @@ import Onboarding2 from '@/app/onboarding/Onboarding-2';
 import Onboarding3 from '@/app/onboarding/Onboarding-3';
 import Onboarding4 from '@/app/onboarding/Onboarding-4';
 import Onboarding5 from '@/app/onboarding/Onboarding-5';
+import Onboarding6 from '@/app/onboarding/Onboarding-6';
 
 export interface OnboardingProps {
     onNext: () => void;
@@ -20,7 +21,7 @@ const OnboardingLogic = () => {
     const router = useRouter();
 
     const nextPage = () => {
-        if (currentPage < 6) {
+        if (currentPage < 5) {
             setCurrentPage(currentPage + 1);
         } else {
             router.push('/dashboard');
@@ -48,17 +49,24 @@ const OnboardingLogic = () => {
             <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 1 ? 'translate-x-0' : '-translate-x-full'}`}>
                 <Onboarding1 onNext={nextPage}/>
             </div>
+            {/*
             <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 2 ? 'translate-x-0' : currentPage < 2 ? 'translate-x-full' : '-translate-x-full'}`}>
                 <Onboarding2 onNext={nextPage}/>
             </div>
+            */}
+            {/*
             <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 3 ? 'translate-x-0' : currentPage < 3 ? 'translate-x-full' : '-translate-x-full'}`}>
                 <Onboarding3 onNext={nextPage}/>
             </div>
-            <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 4 ? 'translate-x-0' : currentPage < 4 ? 'translate-x-full' : '-translate-x-full'}`}>
+            */}
+            <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 2 ? 'translate-x-0' : currentPage < 2 ? 'translate-x-full' : '-translate-x-full'}`}>
                 <Onboarding4 onNext={nextPage}/>
             </div>
-            <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 5 ? 'translate-x-0' : currentPage < 5 ? 'translate-x-full' : '-translate-x-full'}`}>
+            <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 3 ? 'translate-x-0' : currentPage < 3 ? 'translate-x-full' : '-translate-x-full'}`}>
                 <Onboarding5 onNext={nextPage}/>
+            </div>
+            <div className={`absolute inset-0 transition-transform duration-600 ease-in-out ${currentPage === 4 ? 'translate-x-0' : currentPage < 4 ? 'translate-x-full' : '-translate-x-full'}`}>
+                <Onboarding6 onNext={nextPage}/>
             </div>
         </div>
     );
